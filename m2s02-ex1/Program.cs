46a6cc34efae3e1ba9ca7e27aa4f7ca7caadfc98
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-06-08 08:26:17 -0300

    adicionando m2s02 - ex1

---
+++ m2s02-ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("-- [M2S02] Ex 1 - Cálculo Salário Funcionário --");
+string nome = "Marcelo";
+int anoNascimento = 1987;
+float salario = 5287.85f;
+float reajuste = 7.8f;
+float calc = ((salario * reajuste) / 100) + salario;
+
+Console.WriteLine($"Nome: {nome}");
+Console.WriteLine($"Ano de nascimento: {anoNascimento}");
+Console.WriteLine($"Salário com o reajuste: {calc}");
+
+
+
+
