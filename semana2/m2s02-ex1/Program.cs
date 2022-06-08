2962de27233fde280395f11461ce29b5d8d30c19
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-06-08 10:21:27 -0300

    resolução exercicio 1

---
+++ semana2/m2s02-ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--[M2S02] Ex 1 - Cálculo Salário Funcionário --");
+
+string name = "Marcelo";
+int birth = 1987;
+int wage = 5280;
+float readjustment = 0.07f;
+float calc = (wage * readjustment) + wage;
+
+Console.WriteLine($"Nome: {name}");
+Console.WriteLine($"Ano de nascimento: {birth}");
+Console.WriteLine($"Salário com reajuste de 7%: {calc}");
+
---
+++ semana2/m2s02-ex1/README.md
@@ -0,0 +1,4 @@
+##[M2S02] Ex 1 - Cálculo Salário Funcionário
+Crie um programa
+Entrada: o nome, ano de nascimento, salário do funcionário e a % de reajuste.
+Saída: deve mostrar o nome do funcionário, idade e o valor do salário já calculado com o reajuste.
