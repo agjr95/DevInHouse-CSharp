51eb0e6c0c418108e6956dad17b105f51cc95239
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-06-08 14:35:20 -0300

    corrigindo exercicio 1

--- semana2/m2s02-ex1/Program.cs
+++ semana2/m2s02-ex1/Program.cs
@@ -1,13 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("--[M2S02] Ex 1 - Cálculo Salário Funcionário --");
 
-string name = "Marcelo";
-int birth = 1987;
-int wage = 5280;
-float readjustment = 0.07f;
-float calc = (wage * readjustment) + wage;
+Console.WriteLine("Digite o nome do funcionário");
+var name = Convert.ToString(Console.ReadLine());
 
-Console.WriteLine($"Nome: {name}");
-Console.WriteLine($"Ano de nascimento: {birth}");
-Console.WriteLine($"Salário com reajuste de 7%: {calc}");
+Console.WriteLine("Digite o ano em que você nasceu");
+var birth = Convert.ToInt32(Console.ReadLine());
+int year = 2022;
+int age = year - birth;
+
+Console.WriteLine("Digite o seu salário");
+var wage = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite a porcentagem do reajuste");
+decimal readjustment = Convert.ToDecimal(Console.ReadLine());
+
+decimal calc = ((wage * readjustment / 100)) + wage;
+
+Console.WriteLine($"Nome do funcionário: {name}");
+Console.WriteLine($"Idade do funcionário: {age}");
+Console.WriteLine($"Salário com reajuste de {readjustment}%: R$ {calc}");
 
