955ee64d7cfba75f8d40a099207e8800fc0d12d3
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-06-09 11:03:17 -0300

    resolução do exercicio 2

--- semana2/m2s02-ex2/Program.cs
+++ semana2/m2s02-ex2/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿float[] notas = new float[6];
+int notasZero = 0;
+int notasMenorQueSete = 0;
+int notasMaiorQueSete = 0;
+for (int i = 0; i < notas.Length; i++)
+{
+  Console.Write($"Informe a nota do aluno {i + 1} : ");
+  notas[i] = float.Parse(Console.ReadLine());
+  if (notas[i] == 0)
+  {
+    notasZero++;
+  }
+  if (notas[i] > 0 && notas[i] < 7)
+  {
+    notasMenorQueSete++;
+
+  }
+  if (notas[i] >= 7)
+  {
+    notasMaiorQueSete++;
+  }
+
+}
+float media = notas.Average();
+Console.WriteLine($"A media dos alunos foi: {media}");
+Console.WriteLine($"{notasZero} alunos tiraram 0");
+Console.WriteLine($"{notasMenorQueSete} alunos tiraram a baixo de 7");
+Console.WriteLine($"{notasMaiorQueSete} alunos tiraram acima de 7");
 
---
+++ semana2/m2s02-ex2/README.md
@@ -0,0 +1,5 @@
+## [M2S02] Ex 2 - Cálculo Média de Alunos
+
+Crie um programa
+Entrada: notas de 6 alunos e mostre como.
+Saída: deve mostrar a média da nota da prova e o total de quantos tiveram nota maior que 7; nota menor que 7 e maior que 0 e notas igual a 0.
