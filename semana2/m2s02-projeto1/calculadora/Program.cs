060d9cd397ac2a0ff153b96ad589c847bae0d42d
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-06-11 07:37:59 -0300

    corrigindo o namespace

--- semana2/m2s02-projeto1/calculadora/Program.cs
+++ semana2/m2s02-projeto1/calculadora/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-using calculadora;
+﻿using System;
+namespace calculadora
+{
+
+  internal class Program
+  {
+
+    static void Main(string[] args)
+    {
+      Console.Write("\n\n ---- Menu Calculadora ----\n\n");
+
+      foreach (int i in Enum.GetValues(typeof(OpcoesMenu)))
+      {
+        Console.WriteLine($"{i} - {Enum.GetName(typeof(OpcoesMenu), i)}\n\n");
+      }
+      Console.Write("Tecle o número correspondente a ação desejada\n");
+      OpcoesMenu opcao = (OpcoesMenu)int.Parse(Console.ReadLine());
+      Console.WriteLine($"Opção escolhida: {opcao}");
+    }
+  }
+}
+
 
